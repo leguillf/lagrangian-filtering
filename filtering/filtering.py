dbd84ce1752dfdf3ac6648ce266fb8c8e83da670
leguilfl <florian.leguillou@univ-grenoble-alpes.fr>
2020-02-03 19:11:05 +0100

    Same as previous commit

--- filtering/filtering.py
+++ filtering/filtering.py
@@ -24,30 +24,6 @@ from functools import partial
 
 import pathos.pools as pp
 
-def _pickle_method(method):
- 	func_name = method.im_func.__name__
- 	obj = method.im_self
- 	cls = method.im_class
- 	if func_name.startswith('__') and not func_name.endswith('__'): #deal with mangled names
-         cls_name = cls.__name__.lstrip('_')
-         func_name = '_' + cls_name + func_name
- 	return _unpickle_method, (func_name, obj, cls)
-
-def _unpickle_method(func_name, obj, cls):
- 	for cls in cls.__mro__:
-         try:
-             func = cls.__dict__[func_name]
-         except KeyError:
-             pass
-         else:
-             break
- 	return func.__get__(obj, cls)
-
-import copyreg
-import types
-copyreg.pickle(types.MethodType, _pickle_method, _unpickle_method)
-
-
 class LagrangeFilter(object):
     """The main class for a Lagrangian filtering workflow.
 
@@ -799,27 +775,12 @@ class LagrangeFilter(object):
         # or use the full range of times covered by window
         times = self._window_times(times, absolute)
 
-        if len(times)==1:
-            print('Only one timestamp')
-            self._do_filtering([0,times[0]])
-        
-        else:
-            print('run parallel')
-            run_parallel(self,times)
-        
-            #run_parallel(self,times)
-        
+        for idx,time in enumerate(times):
+            self._do_filtering([idx,time])
+            
         print('End of the program')
         
-def run_parallel(my_class,times,number_processes=4):
-    
-    NT = len(times)
-    params = list(enumerate(times))
-    print(params)
-    
-    p = pp.ProcessPool(number_processes)
-    
-    p.map(LagrangeFilter._do_filtering, [my_class]*NT, params)
+
     
     
 
